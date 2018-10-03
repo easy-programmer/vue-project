821631cf1629c8bae2297aa95656cf2ff428716c
ohardcore <ssoguy3@gmail.com>
2018-10-04 01:08:57 +0900

    hellogrid commit

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,15 @@ import HelloGrid from '@/components/HelloGrid'
 import SmGrid from '@/components/smGrid'
 import Movie from '@/components/Movie'
 import VueGoodTablePlugin from 'vue-good-table'
+import BootstrapVue from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vue-good-table/dist/vue-good-table.css'
 
 Vue.use(Router)
 Vue.use(VueGoodTablePlugin)
-
+Vue.use(BootstrapVue)
 
 export default new Router({
   routes: [
